a929356618b1429515c75506661d14afb826ad8a
Felix Pacheco <devfex.19@gmail.com>
2020-08-24 18:07:57 -0400

    ignore

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Customers from "../pages/home";
-import CustomerDetails from "../pages/customer-detail";
-import EditCustomer from "../pages/edit-customer";
-import NewCustomer from "../pages/new-customer";
+import CurrentWeather from "../pages/current-weather";
+// import CustomerDetails from "../pages/customer-detail";
 
 const RouterContainer = () => {
   return (
     <Router>
-      <Route exact path="/" component={Customers} />
       <Switch>
-        <Route exact path="/customer/:dni/edit" component={EditCustomer} />
-        <Route exact path="/customer/:dni" component={CustomerDetails} />
-        <Route exact path="/customer/new" component={NewCustomer} />
+        <Route exact path="/" component={CurrentWeather} />
+        {/* <Route exact path="/customer/:dni/edit" component={EditCustomer} /> */}
       </Switch>
     </Router>
   );
