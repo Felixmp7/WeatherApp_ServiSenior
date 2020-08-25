0589e37e3ee93df6492dc2051d04880776fc0698
Felix Pacheco <devfex.19@gmail.com>
2020-08-24 21:05:56 -0400

    creating files to weather Graphics component

---
+++ src/components/WeatherGraphics.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './WeatherGraphics.css'
+
+const WeatherGraphics = props => {
+  return (
+    <div className="graphicsContainer">
+      
+    </div>
+  )
+}
+
+WeatherGraphics.propTypes = {
+
+}
+
+export default WeatherGraphics
