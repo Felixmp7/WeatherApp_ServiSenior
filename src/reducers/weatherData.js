526cff3fc19b61f680e06111beff060820ed530a
Felix Pacheco <devfex.19@gmail.com>
2020-08-25 12:45:38 -0400

    cleaning

--- src/reducers/weatherData.js
+++ src/reducers/weatherData.js
@@ -1,16 +1,6 @@
 import { GET_WEATHER_DATA } from "../constants/index";
 import { handleActions } from "redux-actions";
 
-// export const saveWeatherData = (state = {}, action) => {
-//   switch (action.type) {
-//     case GET_WEATHER_DATA:
-//       return { ...state, weatherData: [...action.payload] };
-
-//     default:
-//       return state;
-//   }
-// };
-
 export const saveWeatherData = handleActions({
   [GET_WEATHER_DATA]: (state, action) => action.payload
 }, {})
