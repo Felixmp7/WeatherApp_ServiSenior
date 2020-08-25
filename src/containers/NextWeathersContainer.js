e1fb47b131e1400cbd33725f8958dc620fe5395d
Felix Pacheco <devfex.19@gmail.com>
2020-08-24 21:06:09 -0400

    implementing container for next weathers

---
+++ src/containers/NextWeathersContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import NextWeather from '../components/NextWeather';
+
+const NextWeathersContainer = ({nextDaysWeatherData}) => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Grid container className={classes.container} spacing={2}>
+        {
+          nextDaysWeatherData.map( (weather, index) => {
+            return (
+              <NextWeather
+                key={index}
+                weatherIcon={weather.weatherIcon}
+                day={weather.day}
+              />
+            );
+          })
+        }
+      </Grid>
+    </Paper>
+  );
+}
+
+export default NextWeathersContainer;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '18%',
+    display: 'flex'
+  },
+  container: {
+    flexGrow: 1,
+  }
+}));
