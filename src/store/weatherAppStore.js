72e72a25d8ba27796bc36f9d155b2eb3abe9e0fa
Felix Pacheco <devfex.19@gmail.com>
2020-08-25 09:29:02 -0400

    implementing store

---
+++ src/store/weatherAppStore.js
@@ -0,0 +1,14 @@
+// Store
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import storeReducer from "../reducers/index";
+
+const initialState = {};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  storeReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
