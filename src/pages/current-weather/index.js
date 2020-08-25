20a0f057d05c13f9aed82e6d8b85e752748a45c0
Felix Pacheco <devfex.19@gmail.com>
2020-08-24 21:04:26 -0400

    implementing nextWeathersData for example and implementing current page

--- src/pages/current-weather/index.js
+++ src/pages/current-weather/index.js
@@ -1,148 +1,85 @@
-import React from 'react'
-import Header from './components/Header';
-import CustomersList from './containers/CustomersList';
-import Footer from '../../utils/footer/Footer';
-import PropTypes from 'prop-types'
-import Button from "@material-ui/core/Button";
+import React, { Component } from 'react'
+import WeatherContainer from '../../containers/WeatherContainer';
+import Weather from '../../components/Weather';
+import WeatherRightBar from '../../components/WeatherRightBar';
+import NextWeathersContainer from '../../containers/NextWeathersContainer';
 import './index.css';
-import { withRouter } from 'react-router-dom';
 
-const customerData = [
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Pablo Alborán",
-    age: 30,
-    avatar: 'm',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "José Enrique",
-    age: 40,
-    avatar: 'm',
-  },
-  {
-    dni: "6868686",
-    name: "Gabriel Pacheco",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Doris Alonso",
-    dni: "99999",
-    age: 11,
-    avatar: 'w',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Eliana Guerrero",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Arianne Camila",
-    age: 40,
-    avatar: 'w',
-  },
-  {
-    dni: "6868686",
-    name: "Jennifer Marques",
-    age: 10,
-    avatar: 'w',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Jose Daniel Tapiquén",
-    dni: "99999",
-    age: 11,
-    avatar: 'm',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Leonella Berroterán",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Maria Fernanda Gonzalez",
-    age: 40,
-    avatar: 'w',
-  },
-  {
-    dni: "6868686",
-    name: "José Tirado",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Fibra TV",
-    dni: "99999",
-    age: 11,
-    avatar: 'e',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "María Ortiz",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Alejandro Hadyar",
-    age: 40,
-    avatar: 'm',
-  },
-  {
-    dni: "6868686",
-    name: "César Rivas",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Manuel Carrero",
-    dni: "99999",
-    age: 11,
-    avatar: 'm',
-    id: "kxmVQVd",
-  },
-];
+class CurrentWeather extends Component {
+  state = {
+    nextWeatherData: [
+      {
+        day:'Monday',
+        humidity: "30000001",
+        weather: "30000001",
+        cityName: "Pablo Alborán",
+        temperature: 30,
+        wind: "m",
+        weatherIcon: 'Clouds',
+      },
+      {
+        day:'Tuesday',
+        humidity: "500000",
+        weather: "500000",
+        cityName: "José Enrique",
+        temperature: 40,
+        wind: "m",
+        weatherIcon: 'Clear',
+      },
+      {
+        day:'Wednesday',
+        weather: "6868686",
+        cityName: "Gabriel Pacheco",
+        temperature: 10,
+        wind: "m",
+        weatherIcon: 'Thunderstorm',
+        humidity: "XSCfaW5",
+      },
+      {
+        day:'Thursday',
+        cityName: "Doris Alonso",
+        weather: "99999",
+        temperature: 11,
+        wind: "w",
+        weatherIcon: 'Rain',
+        humidity: "kxmVQVd",
+      },
+      {
+        day:'Friday',
+        cityName: "Doris Alonso",
+        weather: "99999",
+        temperature: 11,
+        wind: "w",
+        weatherIcon: 'Snow',
+        humidity: "kxmVQVd",
+      },
+    ],
+    currentWeather: {
+      cityName: "Manuel Carrero",
+      weather: "99999",
+      temperature: 11,
+      wind: "m",
+      weatherIcon: 'Clear',
+      humidity: "kxmVQVd",
+    },
+  };
 
-const CustomerPage = ({headerTitle, contactsData, footer, history}) => {
-
-  const createNewCustomer = () => {
-    history.push('/customer/new');
+  render() {
+    return (
+      <div className="currentWeatherPage">
+        <WeatherContainer>
+          <div className="gridAlignment">
+            <Weather />
+            <WeatherRightBar />
+          </div>
+          <div className="nextDaysContainer">
+            <h3 className="headerText">Next days weather</h3>
+          </div>
+          <NextWeathersContainer nextDaysWeatherData={this.state.nextWeatherData} />
+        </WeatherContainer>
+      </div>
+    );
   }
-  return (
-    <div className="containerHome">
-      <Header title={headerTitle || "Clientes"} />
-      <CustomersList customerData={customerData} />
-      <Footer>
-        <Button variant='outlined' onClick={createNewCustomer}>
-          New Customer
-        </Button>
-      </Footer>
-    </div>
-  );
-}
+};
 
-CustomerPage.propTypes = {
-  headerTitle: PropTypes.string.isRequired,
-  footer: PropTypes.string,
-}
-
-export default withRouter(CustomerPage);
+export default CurrentWeather;
