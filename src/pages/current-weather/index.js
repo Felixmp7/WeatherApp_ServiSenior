fc26dd344b92b0e13ebc6f0a90cb2f0625c02a38
Felix Pacheco <devfex.19@gmail.com>
2020-08-25 09:29:23 -0400

    connecting currentWeather with redux

--- src/pages/current-weather/index.js
+++ src/pages/current-weather/index.js
@@ -3,66 +3,20 @@ import WeatherContainer from '../../containers/WeatherContainer';
 import Weather from '../../components/Weather';
 import WeatherRightBar from '../../components/WeatherRightBar';
 import NextWeathersContainer from '../../containers/NextWeathersContainer';
+import { fetchWeatherData } from '../../actions/index';
 import './index.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class CurrentWeather extends Component {
   state = {
-    nextWeatherData: [
-      {
-        day:'Monday',
-        humidity: "30000001",
-        weather: "30000001",
-        cityName: "Pablo Alborán",
-        temperature: 30,
-        wind: "m",
-        weatherIcon: 'Clouds',
-      },
-      {
-        day:'Tuesday',
-        humidity: "500000",
-        weather: "500000",
-        cityName: "José Enrique",
-        temperature: 40,
-        wind: "m",
-        weatherIcon: 'Clear',
-      },
-      {
-        day:'Wednesday',
-        weather: "6868686",
-        cityName: "Gabriel Pacheco",
-        temperature: 10,
-        wind: "m",
-        weatherIcon: 'Thunderstorm',
-        humidity: "XSCfaW5",
-      },
-      {
-        day:'Thursday',
-        cityName: "Doris Alonso",
-        weather: "99999",
-        temperature: 11,
-        wind: "w",
-        weatherIcon: 'Rain',
-        humidity: "kxmVQVd",
-      },
-      {
-        day:'Friday',
-        cityName: "Doris Alonso",
-        weather: "99999",
-        temperature: 11,
-        wind: "w",
-        weatherIcon: 'Snow',
-        humidity: "kxmVQVd",
-      },
-    ],
-    currentWeather: {
-      cityName: "Manuel Carrero",
-      weather: "99999",
-      temperature: 11,
-      wind: "m",
-      weatherIcon: 'Clear',
-      humidity: "kxmVQVd",
-    },
+
   };
+
+  componentDidMount(){
+    console.log('Fetch')
+    this.props.fetchWeatherData();
+  }
 
   render() {
     return (
@@ -75,11 +29,29 @@ class CurrentWeather extends Component {
           <div className="nextDaysContainer">
             <h3 className="headerText">Next days weather</h3>
           </div>
-          <NextWeathersContainer nextDaysWeatherData={this.state.nextWeatherData} />
+          <NextWeathersContainer nextDaysWeatherData={this.props.nextWeatherData} />
         </WeatherContainer>
       </div>
     );
   }
 };
 
-export default CurrentWeather;
+CurrentWeather.propTypes = {
+  fetchWeatherData: PropTypes.func.isRequired
+}
+
+CurrentWeather.defaultProps = {
+  nextWeatherData: [],
+  currentWeather: {},
+};
+
+const mapDispatch = dispatch => ({
+  fetchWeatherData: () => dispatch(fetchWeatherData())
+})
+
+const mapState = (state) => ({
+  currentWeather: state.currentWeather,
+  nextWeatherData: state.nextWeatherData,
+});
+
+export default connect(mapState, mapDispatch)(CurrentWeather);
