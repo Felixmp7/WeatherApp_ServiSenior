5ec6d72b2fa7f720695b3e070ee01a404d7c3124
Felix Pacheco <devfex.19@gmail.com>
2020-08-25 12:45:54 -0400

    ignore

--- src/store/weatherAppStore.js
+++ src/store/weatherAppStore.js
@@ -1,6 +1,7 @@
 // Store
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+// import promiseMiddleware from 'redux-promise'
 import storeReducer from "../reducers/index";
 
 const initialState = {};
@@ -8,7 +9,7 @@ const initialState = {};
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-  storeReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(thunk))
-);
+         storeReducer,
+         initialState,
+         composeEnhancers(applyMiddleware(thunk))
+       );
