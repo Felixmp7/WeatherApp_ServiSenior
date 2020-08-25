06203a301cc3cf145096dae893bd434450773c0d
Felix Pacheco <devfex.19@gmail.com>
2020-08-25 12:45:00 -0400

    connecting weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 import WeatherGraphics from './WeatherGraphics'
 import './Weather.css'
 
-const Weather = props => {
+const Weather = ({date, cityName, complementName}) => {
   return (
     <div className="cityNameContainer">
-      <h5 className="weatherDate">Today</h5>
-      <h1 className="cityName">Santiago</h1>
-      <h4 className="complementName">Metropolitan Region</h4>
+      <h5 className="weatherDate">{date}</h5>
+      <h1 className="cityName">{cityName}</h1>
+      <h4 className="complementName">{complementName}</h4>
       <WeatherGraphics/>
     </div>
   );
