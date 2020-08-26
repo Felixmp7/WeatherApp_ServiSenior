9c276da47a85c6436f77fa39c891efa0850ad589
Felix Pacheco <devfex.19@gmail.com>
2020-08-26 14:39:44 -0400

    performing nextWeatherContainer to dinamic

--- src/containers/NextWeathersContainer.js
+++ src/containers/NextWeathersContainer.js
@@ -2,30 +2,46 @@ import React, { Component } from 'react'
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import NextDaySimpleWeather from '../components/NextWeather';
+import NextSimpleWeather from '../components/NextWeather';
+import WeatherPerHour from '../components/WeatherPerHour';
 
-const NextWeathersContainer = ({nextDaysWeatherData}) => {
+const NextWeathersContainer = ({
+  nextDaysWeatherData,
+  nextDaysWeather,
+  nextWeathersPerHours,
+  setCurrentView,
+}) => {
   const classes = useStyles();
 
   return (
     <Paper className={classes.root}>
       <Grid container className={classes.container} spacing={2}>
-        {
-          nextDaysWeatherData.map( (day, index) => {
-            return (
-              <NextDaySimpleWeather
-                key={index}
-                weatherIcon={day[0].weather}
-                day={day[0].date}
-                details={day}
-              />
-            );
-          })
-        }
+        {nextDaysWeather
+          ? nextDaysWeatherData.map((day, index) => {
+              return (
+                <NextSimpleWeather
+                  key={index}
+                  weatherIcon={day[0].weather}
+                  day={day[0].day}
+                  details={day}
+                />
+              );
+            })
+          : nextWeathersPerHours.map((item) => {
+              return (
+                <NextSimpleWeather
+                  key={item.id}
+                  setCurrentView={setCurrentView}
+                  id={item.id}
+                  weatherIcon={item.weather}
+                  hour={item.hour}
+                />
+              );
+            })}
       </Grid>
     </Paper>
   );
-}
+};
 
 export default NextWeathersContainer;
 
