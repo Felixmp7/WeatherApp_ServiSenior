c27d0a26b3338fa63b11d53a69d505d04f634c58
Felix Pacheco <devfex.19@gmail.com>
2020-08-26 14:37:24 -0400

    changing name from data to day

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import WeatherGraphics from './WeatherGraphics'
 import './Weather.css'
 
-const Weather = ({date, cityName, complementName}) => {
+const Weather = ({day, cityName, complementName}) => {
   return (
     <div className="cityNameContainer">
-      <h5 className="weatherDate">{date}</h5>
+      <h5 className="weatherday">{day}</h5>
       <h1 className="cityName">{cityName}</h1>
       <h4 className="complementName">{complementName}</h4>
       <WeatherGraphics/>
