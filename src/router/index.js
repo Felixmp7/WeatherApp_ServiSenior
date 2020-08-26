3a1554c1535b4320c6cf05e8cf81fe6ace9e4f61
Felix Pacheco <devfex.19@gmail.com>
2020-08-26 12:34:30 -0400

    passing specific props in router to WeatherDetailsComponent

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CurrentWeather from "../pages/current-weather";
+import WeatherDetails from "../pages/weather-details/WeatherDetails";
 
 const RouterContainer = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/:day" render={props => {
+          return (
+            <WeatherDetails
+              day={props.match.params.day}
+              dayWeatherData={props.location.state}
+            />
+          );
+        }} />
         <Route exact path="/" component={CurrentWeather} />
-        {/* <Route exact path="/customer/:dni/edit" component={EditCustomer} /> */}
       </Switch>
     </Router>
   );
