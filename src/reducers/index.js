a01a7887e99f353ecf0005b717af8c92c43617f5
Felix Pacheco <devfex.19@gmail.com>
2020-08-26 22:05:15 -0400

    importing new error reducer to combine

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { saveWeatherData } from './weatherData';
+import { handleErrorInFetchData } from "./handleErrors";
 
 export default combineReducers({
-  weatherData: saveWeatherData
+  weatherData: saveWeatherData,
+  errorInFetchInitialData: handleErrorInFetchData,
 });
