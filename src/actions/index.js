d6162633ed48348ee06b877201b340f8674e7d14
Felix Pacheco <devfex.19@gmail.com>
2020-08-26 22:03:31 -0400

    implementing action to distpatch error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { GET_WEATHER_DATA } from "../constants/index";
+import { GET_WEATHER_DATA, HANDLE_ERROR } from "../constants/index";
 import { getWeatherData } from "../api/api";
 import moment from 'moment'
 
@@ -76,29 +76,53 @@ const groupBy = (array, key) => {
 };
 
 const fetchWeatherDataToApi = async () => {
-
+  console.log("fetchWeatherDataToApi");
   try {
     const response = await getWeatherData();
     // console.log(response);
     if (response.status) {
       return response.data;
     }
+    else {
+      return 'ERROR';
+    }
   } catch (error) {
     console.log(error); 
   }
 }
 
+const getWeatherAction = (data) => ({
+  type: GET_WEATHER_DATA,
+  payload: transformAPIData(data),
+});
+
+const errorAction = {
+  type: HANDLE_ERROR,
+  payload: {error: true},
+};
+
 
 export const fetchWeatherData = () => {
+  console.log('Intentando hacer el fetch')
   return async (dispatch, getState) => {
-    const prevState = getState();
-    if (prevState.weatherData.currentWeather) {
-      return ;
+    try {
+      const prevState = getState();
+      if (prevState.weatherData.currentWeather) {
+        console.log("No hacer nada");
+        return ;
+      }
+      const data = await fetchWeatherDataToApi();
+      console.log(data)
+      if (data !== 'ERROR') {
+        console.log('hacer el Fetch')
+        dispatch(getWeatherAction(data));
+      } else {
+        console.log('Lanzar el error')
+        dispatch(errorAction);
+      }
+    } catch (error) {
+      console.log(error);
+      dispatch(errorAction);
     }
-    const data = await fetchWeatherDataToApi();
-    dispatch({
-      type: GET_WEATHER_DATA,
-      payload: transformAPIData(data),
-    });
   };
 }
