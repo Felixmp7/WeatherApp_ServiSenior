5d95863baf87b1a96d604fb282a866d1eb628d4b
Felix Pacheco <devfex.19@gmail.com>
2020-08-27 11:34:32 -0400

    testing

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import WeatherGraphics from './WeatherGraphics'
+// import WeatherGraphics from './WeatherGraphics'
+import WeatherGraphics from './WeatherGraphics_Test'
 import './Weather.css'
 
-const Weather = ({day, cityName, complementName}) => {
+const Weather = ({
+  day,
+  cityName,
+  complementName,
+  getVariationTempForNextDays,
+}) => {
   return (
     <div className="cityNameContainer">
       <h5 className="weatherday">{day}</h5>
       <h1 className="cityName">{cityName}</h1>
       <h4 className="complementName">{complementName}</h4>
-      <WeatherGraphics/>
+      <WeatherGraphics
+        getVariationTempForNextDays={getVariationTempForNextDays ? true : false}
+      />
     </div>
   );
-}
+};
 
 Weather.propTypes = {
   day: PropTypes.string.isRequired,
