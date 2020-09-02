e056d2a26417f129a3a66ba3e795b8cf68eb681f
Felix Pacheco <devfex.19@gmail.com>
2020-09-02 16:56:45 -0400

    handle validation in location state to render error page not found component

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import CurrentWeather from "../pages/current-weather";
 import WeatherDetails from "../pages/weather-details/WeatherDetails";
+import ErrorPage from '../pages/error404/ErrorPage';
 
 const RouterContainer = () => {
   return (
-    <Router>
+    <Router
+     basename="/"
+    >
       <Switch>
-        <Route exact path="/:day" render={props => {
-          return (
-            <WeatherDetails
-              day={props.match.params.day}
-              dayWeatherData={props.location.state}
-            />
-          );
-        }} />
         <Route exact path="/" component={CurrentWeather} />
+        <Route exact path="/:day" render={props => {
+          // console.log(props)
+          if (props.location.state) {
+            return (
+              <WeatherDetails
+                day={props.match.params.day}
+                dayWeatherData={props.location.state}
+              />
+            );
+          }
+          else return <Route component={ErrorPage} />;
+          }} />
+        <Route component={ErrorPage} />
       </Switch>
     </Router>
   );
